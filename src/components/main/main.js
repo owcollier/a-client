fcaa28556bb526767d739dd1ed2e3c805e97c4b9
Owcollier <owcollier@gmail.com>
2018-06-09 19:56:54 -0400

    HeaderHero component imported and rendered in main.js

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import HeaderHero from '../header-hero/header-hero';
 
 export function Main(props) {
   return (
     <main className="app-main-content">
+      <HeaderHero />
     </main>
   )
 }
