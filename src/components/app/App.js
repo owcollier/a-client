c6b5731329055a5a406aab47d6427cc25b96dcee
Owcollier <owcollier@gmail.com>
2018-06-09 18:29:29 -0400

    tweaks to index.js to correctly import App top-level component, tweaks to app.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
 
+import TopNav from '../top-nav/top-nav';
+
 class App extends Component {
   render() {
     return (
