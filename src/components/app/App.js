d18a3b8f90fa673222c52e5c1ddaa227724e30da
Owcollier <owcollier@gmail.com>
2018-06-09 19:12:47 -0400

    building up the TopNav component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import TopNav from '../top-nav/top-nav';
 import './app.css';
 
-import TopNav from '../top-nav/top-nav';
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
+        <TopNav />
       </div>
     );
   }
---
+++ src/components/top-nav/top-nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export function TopNav(props) {
+  return (
+    <div>
+      <a className="app-nav-logo"> This will be the logo :) </a>
+      <nav className="app-nav">
+        <ul>
+          <li className="app-nav-item"></li>
+          <li className="app-nav-item"></li>
+          <li className="app-nav-item"></li>
+          <li className="app-nav-item"></li>
+          <li className="app-nav-item"></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default TopNav;
