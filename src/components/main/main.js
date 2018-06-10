a03db4aed0271dc667d99d65724257394bcc8651
Owcollier <owcollier@gmail.com>
2018-06-09 21:54:51 -0400

    imported ContentColumn in main.js, rendering ContentColumn

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeaderHero from '../header-hero/header-hero';
+import ContentColumn from '../content-column/content-column';
 
 export function Main(props) {
   return (
     <main className="app-main-content">
       <HeaderHero />
+      <ContentColumn />
     </main>
   )
 }
