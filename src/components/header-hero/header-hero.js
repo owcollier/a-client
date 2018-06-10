309f3b2c4aed3c097a9547f0ed87a338046809da
Owcollier <owcollier@gmail.com>
2018-06-09 20:32:58 -0400

    added content to HeaderHero component

--- src/components/header-hero/header-hero.js
+++ src/components/header-hero/header-hero.js
@@ -4,6 +4,7 @@ export function HeaderHero(props) {
   return (
     <header className="app-hero">
       <div className="single-column-layout">
+        <h2 className="hero-title">I want to join a team of developers and designers dedicated to creating products people love to use</h2>
       </div>
     </header>
   )
