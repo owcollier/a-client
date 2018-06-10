8af37a065face5706e3bfdd944b1f08372cfb559
Owcollier <owcollier@gmail.com>
2018-06-09 21:53:25 -0400

    building ContentColumn component

---
+++ src/components/content-column/content-column.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function ContentColumn(props) {
+  return (
+    <section className="single-column-layout" id="meat">
+      <p>hello world</p>
+    </section>
+  )
+}
+
+export default ContentColumn;
