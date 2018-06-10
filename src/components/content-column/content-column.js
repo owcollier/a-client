333a25eac2c1ddf8cf301eb3ce97a91795ac55ff
Owcollier <owcollier@gmail.com>
2018-06-10 17:19:12 -0400

    adding more content to the ContentColumn component, fleshing out the jsx

--- src/components/content-column/content-column.js
+++ src/components/content-column/content-column.js
@@ -6,6 +6,12 @@ export function ContentColumn(props) {
       <p className="view-content-column">
        <a>Back to reality</a>
       </p>
+      <h1 className="title-section">Full Stack Web Developer in New York</h1>
+      <div className="content-show-description">
+      <p>What I'm looking for</p>
+      <h3>What I can offer</h3>
+      <p>Details about what I can offer</p>
+      </div>
     </section>
   )
 }
