9718581affdafb319e155b394b269770ded24b2d
Owcollier <owcollier@gmail.com>
2018-06-10 18:46:09 -0400

    more content added to ContentColumn component

--- src/components/content-column/content-column.js
+++ src/components/content-column/content-column.js
@@ -8,10 +8,13 @@ export function ContentColumn(props) {
       </p>
       <h1 className="title-section">Full Stack Web Developer in New York</h1>
       <div className="content-show-description">
-      <p>What I'm looking for</p>
-      <h3>What I can offer</h3>
-      <p>Details about what I can offer</p>
+        <p>What I'm looking for</p>
+        <h3>What I can offer</h3>
+        <p>Details about what I can offer</p>
       </div>
+      <h3>About me</h3>
+      <p>Some stuff about me</p>
+      <p>A concluding statement about me :)</p>
     </section>
   )
 }
