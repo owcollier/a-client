ef9a27d50649f11cdd19370912a4bd83420e398a
Owcollier <owcollier@gmail.com>
2018-06-11 13:34:38 -0400

    styles for top nav updated

--- src/components/top-nav/top-nav.js
+++ src/components/top-nav/top-nav.js
@@ -3,13 +3,12 @@ import React from 'react';
 export function TopNav(props) {
   return (
     <header className="app-header">
-      <a className="app-nav-logo"> This will be the logo :) </a>
+      <a className="app-nav-logo">Wade Collier</a>
       <nav className="app-nav">
         <ul>
-          <li className="app-nav-item">stuff</li>
-          <li className="app-nav-item">stuff</li>
-          <li className="app-nav-item">stuff</li>
-          <li className="app-nav-item">stuff</li>
+          <li className="app-nav-item">Portfolio</li>
+          <li className="app-nav-item">LinkedIn</li>
+          <li className="app-nav-item">GitHub</li>
           <li className="app-nav-item">
             <a className="app-nav-link app-nav-contact-me">Contact me</a>
           </li>
