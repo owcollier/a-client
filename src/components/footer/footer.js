99aaa268e6fe1191ea36f7cde9720911a436f0c0
Owcollier <owcollier@gmail.com>
2018-06-11 09:16:27 -0400

    update to Footer component, basic jsx structure set up

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,6 +3,34 @@ import React from 'react';
 export function Footer(props) {
   return (
     <footer className="app-footer">
+      <div className="footer-content">
+        <ul className="footer-nav-groups">
+          <li className="footer-nav-group">
+            <h3 className="footer-nav-group-title">Footer stuff</h3>
+            <ul className="footer-nav-list">
+              <li><a>Something</a></li>
+              <li><a>Something</a></li>
+              <li><a>Something</a></li>
+            </ul>
+          </li>
+          <li className="footer-nav-group">
+            <h3 className="footer-nav-group-title">Footer stuff</h3>
+            <ul className="footer-nav-list">
+              <li><a>Something</a></li>
+              <li><a>Something</a></li>
+              <li><a>Something</a></li>
+            </ul>
+          </li>
+          <li className="footer-nav-group">
+            <h3 className="footer-nav-group-title">Footer stuff</h3>
+            <ul className="footer-nav-list">
+              <li><a>Something</a></li>
+              <li><a>Something</a></li>
+              <li><a>Something</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </footer>
   )
 }
