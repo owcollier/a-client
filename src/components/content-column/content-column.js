6d0826aabed0366756deb4f8e91d1596ae0b488f
Owcollier <owcollier@gmail.com>
2018-06-13 09:29:32 -0400

    some css added and experimenting with inline span tag for faux icon in the top-nav, content column updated

--- src/components/content-column/content-column.js
+++ src/components/content-column/content-column.js
@@ -9,7 +9,8 @@ export function ContentColumn(props) {
       </p>
       <h1 className="title-section">Full Stack Web Developer in New York</h1>
       <div className="content-show-description">
-        <p>What I'm looking for</p>
+        <p>I'm looking to join a high quality team of designers and developers to apply my skills, continue growing as a developer, and start working with clients to provide . As a recent graduate of Thinkful's full time Engineering Immersion program, I have acquired working experience building front end web clients with JavaScript, HTML, and CSS, running web servers and writing RESTFUL apis using Node.js/Express, and integrating both relational and non-relational databases using MongoDB, and PostgreSQL. Beyond this full stack foundation, I have worked intensively and continue to build web applications with MVC frameworks - specifically with React.</p>
+        <p>We're looking for a developer experienced with React Native to join our team in New York City. You’ll work with our clients to build great products that delight users while using weekly investment time to improve ourselves, our company, and our community.</p>
         <h3>What I can offer</h3>
         <p>Details about what I can offer</p>
       </div>
--- src/components/top-nav/top-nav.js
+++ src/components/top-nav/top-nav.js
@@ -3,12 +3,18 @@ import React from 'react';
 export function TopNav(props) {
   return (
     <header className="app-header">
-      <a className="app-nav-logo">Wade Collier</a>
+      <a className="app-nav-logo"><span className="app-nav-bubble">&nbsp;Hi&nbsp;</span> thoughtbot! My name is Wade Collier</a>
       <nav className="app-nav">
         <ul>
-          <li className="app-nav-item">Portfolio</li>
-          <li className="app-nav-item">LinkedIn</li>
-          <li className="app-nav-item">GitHub</li>
+          <li className="app-nav-item">
+            <a className="app-nav-link">Portfolio</a>
+          </li>
+          <li className="app-nav-item">
+            <a className="app-nav-link">LinkedIn</a>
+          </li>
+          <li className="app-nav-item">
+            <a className="app-nav-link">GitHub</a>
+          </li>
           <li className="app-nav-item">
             <a className="app-nav-link app-nav-contact-me">Contact me</a>
           </li>
