8d0bfae71c30310655dadef5d00830d3717d4da8
Owcollier <owcollier@gmail.com>
2018-06-16 18:12:55 -0400

    more content to ContentColumn, responsive css for all components within html body tag incoming

--- src/components/content-column/content-column.js
+++ src/components/content-column/content-column.js
@@ -9,15 +9,16 @@ export function ContentColumn(props) {
       </p>
       <h1 className="title-section">Full Stack Web Developer in New York</h1>
       <div className="content-show-description">
-        <p>I'm looking to join a high quality team of designers and developers to apply my skills, continue learning, and contribute in a meaningful way to clients, my community, and a company I can believe in.</p>
-        <p>Thoughtbot seems like a wonderful place to continue my career as a developer. I'm impressed by the work you guys have done - the 3d martial arts visualization app is particularly cool (I love implementing WebGL and 3d animation in HTML5 Canvas) - and am further impressed with the company's mission, benefits, and focus on personal development every week.</p>
+        <p>Hi thoughtbot! My name is Wade Collier and I'm looking for an opportunity to apply my skills, continue learning, and contribute in a meaningful way to clients, my community, and a company I can believe in.</p>
+        <p>Thoughtbot seems like a wonderful place to continue my career as a developer. I'm impressed by the work you've done - the 3d martial arts visualization app is particularly cool (I love implementing WebGL and 3d animation in HTML5 Canvas) - and am further impressed with the company's mission, benefits, and focus on personal development every week.</p>
         <h3>What I can offer</h3>
-        <p>As a recent graduate of Thinkful's full time Engineering Immersion program, I've acquired working experience building front end web clients with JavaScript, HTML, and CSS. I also have experience running web servers and writing RESTFUL APIs on the back end using Node.js/Express, and integrating both relational and non-relational databases using MongoDB, and PostgreSQL respectively.</p>
-        <p> Beyond this full stack foundation, I've worked intensively with React. I'm very well versed in React/Redux, and would love to have an opportunity to extend that knowledge into the realm of React Native. I've spent a lot of time building mobile first web applications and building native apps is the next level. I'm sure that my knowledge of React's component-based architecture, state management,and workflow will translate quite well into React Native if given the opportunity to bring my own ability to learn new technologies/frameworks quickly and efficiently.</p>
+        <p>As a recent graduate of Thinkful's full time Engineering Immersion program, I've acquired working experience building front end web clients with JavaScript, HTML, CSS, and React/Redux. I also have experience running web servers and writing RESTFUL APIs on the back end using Node.js/Express, and integrating both relational and non-relational databases using MongoDB, and PostgreSQL respectively.</p>
+        <h3>But Wade, we're looking for React Native developers!</h3>
+        <p> I see that you are currently hiring for React Native developers, and although my current experience with React and Redux lies in web application territory, I still believe that I can be a great addition to your team. I've worked intensively with React and Redux, and am looking for an opportunity to extend that experience into the realm of React Native. I know that my knowledge of React's component-based architecture, state management, and workflow will translate quite well into React Native if given the opportunity to bring my own ability to learn new technologies/frameworks quickly and efficiently to the table. I've already built several mobile first web applications, and developing native applications is just the next step. I have full confidence that I can productively join a React Native development team and effectively learn from peers.</p>
       </div>
-      <h3>On a more personal note</h3>
+      {/* <h3>On a more personal note</h3>
       <p>Some stuff about me</p>
-      <p>A concluding statement about me :)</p>
+      <p>A concluding statement about me :)</p> */}
       <PhotoGrid />
       <h3>My Benefits</h3>
       <p>A quick statement</p>
