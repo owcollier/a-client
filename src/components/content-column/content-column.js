e5c6400b64503147eeab893d0f4ca12f61e67326
Owcollier <owcollier@gmail.com>
2018-06-16 00:19:28 -0400

    content added in ContentColumn component

--- src/components/content-column/content-column.js
+++ src/components/content-column/content-column.js
@@ -9,10 +9,11 @@ export function ContentColumn(props) {
       </p>
       <h1 className="title-section">Full Stack Web Developer in New York</h1>
       <div className="content-show-description">
-        <p>I'm looking to join a high quality team of designers and developers to apply my skills, continue learning, and begin meaningful relationships with clients to provide professional-grade digital products.</p>
-        <p>Thoughtbot seems like a wonderful place to continue my career as a developer.</p>
+        <p>I'm looking to join a high quality team of designers and developers to apply my skills, continue learning, and contribute in a meaningful way to clients, my community, and a company I can believe in.</p>
+        <p>Thoughtbot seems like a wonderful place to continue my career as a developer. I'm impressed by the work you guys have done - the 3d martial arts visualization app is particularly cool (I love implementing WebGL and 3d animation in HTML5 Canvas) - and am further impressed with the company's mission, benefits, and focus on personal development every week.</p>
         <h3>What I can offer</h3>
-        <p>As a recent graduate of Thinkful's full time Engineering Immersion program, I have acquired working experience building front end web clients with JavaScript, HTML, and CSS. I also have experience running web servers and writing RESTFUL APIs on the back end using Node.js/Express, and integrating both relational and non-relational databases using MongoDB, and PostgreSQL respectively. Beyond this full stack foundation, I have worked intensively with and continue to build web applications using MVC frameworks such as React.</p>
+        <p>As a recent graduate of Thinkful's full time Engineering Immersion program, I've acquired working experience building front end web clients with JavaScript, HTML, and CSS. I also have experience running web servers and writing RESTFUL APIs on the back end using Node.js/Express, and integrating both relational and non-relational databases using MongoDB, and PostgreSQL respectively.</p>
+        <p> Beyond this full stack foundation, I've worked intensively with React. I'm very well versed in React/Redux, and would love to have an opportunity to extend that knowledge into the realm of React Native. I've spent a lot of time building mobile first web applications and building native apps is the next level. I'm sure that my knowledge of React's component-based architecture, state management,and workflow will translate quite well into React Native if given the opportunity to bring my own ability to learn new technologies/frameworks quickly and efficiently.</p>
       </div>
       <h3>On a more personal note</h3>
       <p>Some stuff about me</p>
