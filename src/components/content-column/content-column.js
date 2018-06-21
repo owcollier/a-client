16fef540a6a7596dba56b37f4a210cd3dc5dce57
Owcollier <owcollier@gmail.com>
2018-06-21 09:56:39 -0400

    css updated and contact button added to bottom of ContentColumn component

--- src/components/content-column/content-column.js
+++ src/components/content-column/content-column.js
@@ -55,8 +55,9 @@ export function ContentColumn(props) {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
         </li>
       </ul>
-      <p></p>
-      <p></p>
+      <p className="big-contact-me">
+        <a className="primary-button" href="http://wadecollier.com/" target="_blank" rel="noopener noreferrer">Contact me</a>
+      </p>
     </section>
   )
 }
